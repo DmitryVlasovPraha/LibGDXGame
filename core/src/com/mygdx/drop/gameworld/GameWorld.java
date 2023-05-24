15b02ae0d13a3e37a92b6a2c61a25a7a6ec3e428
Dmitrii Vlasov <ring.72.dmitry@yandex.ru>
2023-05-24 14:40:12 +0200

    Create Rectangle

--- core/src/com/mygdx/drop/gameworld/GameWorld.java
+++ core/src/com/mygdx/drop/gameworld/GameWorld.java
@@ -1,9 +1,24 @@
 package com.mygdx.drop.gameworld;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
 
 public class GameWorld {
 
+    private Rectangle rect = new Rectangle(0, 0, 17, 12);
+
     public void update(float delta) {
         Gdx.app.log("GameWorld", "update");
+        rect.x++;
+
+        if (rect.x > 137) {
+            rect.x = 0;
+            //rect.y = 50;
+        }
+    }
+
+    /* Метод для доступа к react
+    * */
+    public Rectangle getRect() {
+        return rect;
     }
 }
